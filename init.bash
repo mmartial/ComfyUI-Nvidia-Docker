abb9f9031b640fa6640737a5a142b1117eadc52e
Martial M  <7586284+mmartial@users.noreply.github.com>
2024-08-19 22:45:50 -0400

    Sync directories instead of created some, safer in the long term + added some verbose printout at script start

--- init.bash
+++ init.bash
@@ -2,24 +2,45 @@
 
 set -e
 
-if [ ! -f /etc/comfy_dir ]; then
-  echo "/etc/comfy_dir missing, exiting"
+error_exit() {
+  echo $*
   exit 1
-fi
+}
 
+it=/etc/comfy_base.txt
+if [ ! -f $it ]; then error_exit "$it missing, exiting"; fi
+echo "-- Base image details (from $it):"; cat $it
 
-COMFY_DIR=`cat /etc/comfy_dir`
+it=/etc/comfy_main.txt
+if [ ! -f $it ]; then error_exit "$it missing, exiting"; fi
+echo "-- Main image details (from $it):"; cat $it
+
+it=/etc/comfy_dir
+if [ ! -f $it ]; then error_exit "$it missing, exiting"; fi
+COMFY_DIR=`cat $it`
 echo "-- COMFY_DIR: \"${COMFY_DIR}\""
+if test -z ${COMFY_DIR}; then error_exit "Empty COMFY_DIR variable"; fi
 
-COMFY_VERSION=`cat /etc/comfy_version`
+it=/etc/comfy_userdir
+if [ ! -f $it ]; then error_exit "$it missing, exiting"; fi
+COMFY_USERDIR=`cat $it`
+if test -z ${COMFY_USERDIR}; then error_exit "Empty COMFY_USERDIR variable"; fi
+echo "-- Content directory: \"${COMFY_USERDIR}\""
+find ${COMFY_USERDIR} -type f -exec ls -l {} \;
+
+
+it=/etc/comfy_version
+if [ ! -f $it ]; then error_exit "$it missing, exiting"; fi
+COMFY_VERSION=`cat $it`
+if test -z ${COMFY_VERSION}; then error_exit "Empty COMFY_VERSION variable"; fi
 echo "-- ComfyUI version: \"${COMFY_VERSION}\""
 
-# we are running as comfy, see if we need to modify UID/GID
+# we are running with some given UID/GID, do we need to modify UID/GID
 comfy_uid=`id -u`
 comfy_gid=`id -g`
 
 do_change="False"
-# Not checking the passed WANTED_ value (if any)
+# Not checking the validity (or security) of passed WANTED_ value
 
 do_gid="False"
 if [ ! -z "$WANTED_GID" -a "$WANTED_GID" != "$comfy_gid" ]; then
@@ -51,14 +72,14 @@ echo "== user -- uid: $new_uid / gid: $new_gid"
 if [ ! -z "$WANTED_GID" -a "$WANTED_GID" != "$new_gid" ]; then echo "Wrong GID ($new_gid), exiting"; exit 0; fi
 if [ ! -z "$WANTED_UID" -a "$WANTED_UID" != "$new_uid" ]; then echo "Wrong UID ($new_uid), exiting"; exit 0; fi
 
-# This should be the only directory mounted 
-cd /home/comfy/mnt
-for dir in HF data/input data/output data/temp user models/checkpoints models/clip models/clip_vision models/configs models/controlnet models/diffusers models/embeddings models/gligen models/hypernetworks models/loras models/photomaker models/style_models models/unet models/upscale_models models/vae models/vae_approx custom_nodes; do
-  if [ ! -d $dir ]; then echo "-- Attempting to create directory $dir"; mkdir -p $dir; fi
-  if [ ! -d $dir ]; then echo "** Unable to create directory $dir -- exiting"; exit 1; fi 
-done
+# Make the rsync source directory owned by the expected UID/GID
+sudo chown -R ${WANTED_UID}:${WANTED_GID} ${COMFY_USERDIR}
+
+# /home/comfy/mnt/ is mounted to the `docker run [...] -v` and is the only directory we should write to 
+# rsync the prepared directory, doing its best to not overwrite existing files (using "update" keep the most recent) or remove files that are already in the destination
+rsync -avRuh  ${COMFY_USERDIR}/./* /home/comfy/mnt/. || error_exit "rsync failed"
 
 # Full list of CLI options at https://github.com/comfyanonymous/ComfyUI/blob/master/comfy/cli_args.py
 cd ${COMFY_DIR}
 echo "-- ComfyUI version: \"${COMFY_VERSION}\""
-python3 main.py --listen 0.0.0.0 --disable-auto-launch --output-directory /home/comfy/mnt/data/output --temp-directory /home/comfy/mnt/data/temp --input-directory /home/comfy/mnt/data/input
+python3 ./main.py --listen 0.0.0.0 --disable-auto-launch --temp-directory /home/comfy/mnt/data/temp
